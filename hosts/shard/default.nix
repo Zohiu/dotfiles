cbc3e8c01e11c3cec57f4af642a0097dcd1527e8
Zohiu <mail@zohiu.de>
2025-09-06 12:25:53 +0200

    remove old option

--- hosts/shard/default.nix
+++ hosts/shard/default.nix
@@ -67,44 +67,44 @@
   };
 
   # Enable fw-fanctrl (framework fancontrol)
-  programs.fw-fanctrl.enable = true;
+  # programs.fw-fanctrl.enable = true;
 
   # Add a custom config
-  programs.fw-fanctrl.config = {
-    defaultStrategy = "lazy";
-    strategies = {
-      "lazy" = {
-        fanSpeedUpdateFrequency = 5;
-        movingAverageInterval = 30;
-        speedCurve = [
-          {
-            temp = 0;
-            speed = 0;
-          }
-          {
-            temp = 40;
-            speed = 15;
-          }
-          {
-            temp = 65;
-            speed = 25;
-          }
-          {
-            temp = 70;
-            speed = 35;
-          }
-          {
-            temp = 75;
-            speed = 50;
-          }
-          {
-            temp = 85;
-            speed = 100;
-          }
-        ];
-      };
-    };
-  };
+#   programs.fw-fanctrl.config = {
+#     defaultStrategy = "lazy";
+#     strategies = {
+#       "lazy" = {
+#         fanSpeedUpdateFrequency = 5;
+#         movingAverageInterval = 30;
+#         speedCurve = [
+#           {
+#             temp = 0;
+#             speed = 0;
+#           }
+#           {
+#             temp = 40;
+#             speed = 15;
+#           }
+#           {
+#             temp = 65;
+#             speed = 25;
+#           }
+#           {
+#             temp = 70;
+#             speed = 35;
+#           }
+#           {
+#             temp = 75;
+#             speed = 50;
+#           }
+#           {
+#             temp = 85;
+#             speed = 100;
+#           }
+#         ];
+#       };
+#     };
+#   };
 
   # Home Manager overrides
   home-manager.users.samy.wayland.windowManager.hyprland.settings = {
