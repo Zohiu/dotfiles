81c63d5128f248be35566bb2512e3476bf8f8325
Zohiu <mail@zohiu.de>
2025-09-08 00:59:48 +0200

    shard: power usage optimization

--- hosts/shard/default.nix
+++ hosts/shard/default.nix
@@ -30,11 +30,12 @@
   # Latptop adjustments
   services.cpupower-gui.enable = true;
   powerManagement.cpuFreqGovernor = "schedutil";
-  services.power-profiles-daemon.enable = false; # Conflicts with tlp
   powerManagement.powertop.enable = true;
   services.thermald.enable = true;
+
+  services.power-profiles-daemon.enable = false;
   services.tlp = {
-    enable = false;
+    enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
@@ -47,24 +48,36 @@
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 20;
 
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 80;
-      STOP_CHARGE_THRESH_BAT0 = 95;
+      START_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 97;
 
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      RUNTIME_PM_ON_BAT = "auto";
     };
   };
 
-  services.auto-cpufreq.enable = true; # Might conflict with tlp, but is allowed to be used at the same time.
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-  };
+  boot.kernelParams = [
+    # Tickless / scheduler tuning
+    "nohz_full=1-12"          # Replace N with your last CPU (exclude CPU0)
+    "rcu_nocbs=1-12"          # Offload RCU callbacks from isolated CPUs
+    # AMD GPU power management
+    "amdgpu.dc=1"
+    "amdgpu.deep_color=1"
+    "amdgpu.runpm=1"
+  ];
+
+  #services.auto-cpufreq.enable = false; # Might conflict with tlp, but is allowed to be used at the same time.
+  #services.auto-cpufreq.settings = {
+  #  battery = {
+  #    governor = "powersave";
+  #    turbo = "never";
+  #  };
+  #  charger = {
+  #    governor = "performance";
+  #    turbo = "auto";
+  #  };
+  #};
 
   # Enable fw-fanctrl (framework fancontrol)
   # programs.fw-fanctrl.enable = true;
@@ -113,7 +126,7 @@
     ];
 
     monitor = [
-      ", preferred, auto, auto"
+      "eDP-1, 2880x1920@60, auto, auto"
     ];
   };
 
